93e130982b62ee2012093c5a0e6c8dab135ac856
matthewshan <matthewshan99@gmail.com>
2020-05-04 18:42:09 -0400

    Rotate Array Self Solution

---
+++ Array/rotate.py
@@ -0,0 +1,32 @@
+from typing import List
+
+class Solution:
+    # Matt's First Approach - 60ms (Beats 81%)
+    # Runtime: o(n)
+    # Space: o(n)
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        length = len(nums)
+        if length >= 2:
+            pivot = (length - k) % length 
+            result = [nums[pivot]]
+            
+            i = (pivot+1)%length
+            while i != pivot:
+                result.append(nums[i])
+                i = (i + 1) % length 
+
+            for j in range(length):
+                nums[j] = result[j]
+        #==============================
+        return nums
+        
+
+s = Solution()
+print("Expected: [5,6,7,1,2,3,4] Actual: ", s.rotate([1,2,3,4,5,6,7], 3))
+print("Expected: [3,99,-1,-100] Actual: ", s.rotate([-1,-100,3,99], 2))
+print("Expected: [0] Actual: ", s.rotate([0], 1))
+print("Expected: [2, 0, 1] Actual: ", s.rotate([0, 1, 2], 1))
+print("Expected: [1, 2] Actual: ", s.rotate([1, 2], 0))
